b2e925e0ac4b8eb70d168d71638135c6fab4e5e6
Sven Kratz <Sven Kratz@localhost>
2011-03-03 11:43:53 +0100

    added VisionModules project

---
+++ VisionModules/Action/MotionDetector.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Runtime.Serialization;
+using System.Windows;
+using MayhemCore;
+using MayhemCore.ModuleTypes;
+
+namespace VisionModules
+{
+    [Serializable]
+    public class MotionDetector : ActionBase, IWpf, ISerializable
+    {
+        
+        public MotionDetector()
+            : base("Motion Detector", "Detects when there is motion in the frame")
+        {
+            Setup();
+        }
+        
+        public Setup() {
+          hasConfig = true;
+          
+          SetConfigString();
+        }
+
+        protected void SetConfigString()
+        {
+            ConfigString = String.Format("Configuration Message");
+        }
+
+        public void WpfConfig() {
+            var window = new VisionConfig(); // pass the parameters to initially populate the window in the constructor
+            window.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            
+            if (window.ShowDialog() == true) {
+
+                // Grab data from the window variable and store
+                // it in this class
+                
+                SetConfigString();
+            }
+        }
+
+        public override void Enable() {
+            base.Enable();
+        }
+
+        public override void Disable() {
+            base.Disable();
+        }
+
+        #region Serialization
+
+        public MotionDetector(SerializationInfo info, StreamingContext context) 
+            : base (info, context)
+        {
+            // Code goes here
+            Setup();
+        }
+
+        public new void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            base.GetObjectData(info, context);
+            // code goes here
+        }
+        #endregion
+    }
+}
