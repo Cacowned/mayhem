26a48d6c9213f06c078cc9d302c140627e390af1
Sven Kratz <Sven Kratz@localhost>
2011-03-03 14:21:08 +0100

    MotionDetectorConfig.xaml.cs preview image (untested)

--- VisionModules/Wpf/MotionDetectorConfig.xaml.cs
+++ VisionModules/Wpf/MotionDetectorConfig.xaml.cs
@@ -11,6 +11,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MayhemOpenCVWrapper;
+using System.Diagnostics;
+using System.Drawing;
 
 namespace VisionModules.Wpf
 {
@@ -19,15 +22,102 @@ namespace VisionModules.Wpf
     /// </summary>
     public partial class MotionDetectorConfig : Window
     {
+
+        private MayhemImageUpdater i = MayhemImageUpdater.Instance;
+        private MayhemImageUpdater.ImageUpdateHandler imageUpdateHandler;
+
+        private delegate void SetCameraImageSource();
+
         public MotionDetectorConfig()
         {
             InitializeComponent();
+
+            // image1 = new System.Windows.Controls.Image();
+
+            imageUpdateHandler = new MayhemImageUpdater.ImageUpdateHandler(i_OnImageUpdated);
+
+            // if the image update isn't running yet, start it (could be dangerous) 
+            if (i.running == false)
+            {
+                i.StartFrameGrabbing();
+            }
+
         }
+
+       
+         /**<summary>
+         * Register / De-Register the image update handler if the window is visible / invisible
+         * </summary>
+         */ 
+        private void Window_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (this.IsVisible)
+            {
+                i.OnImageUpdated += imageUpdateHandler;
+            }
+            else
+            {
+                i.OnImageUpdated -= imageUpdateHandler;
+            }
+        }
+
 
         protected override void OnInitialized(EventArgs e)
         {
             base.OnInitialized(e);
         }
+
+        /**<summary>
+         * Invokes image source setting when a new image is available from the update handler. 
+         * </summary>
+         */
+        public void i_OnImageUpdated(object sender, EventArgs e)
+        {
+            Dispatcher.Invoke(new SetCameraImageSource(SetCameraImageSource_), null);
+        }
+
+        /**<summary>
+         * Does the actual drawing of the camera image to the WPF image object in the config window. 
+         * </summary>
+         */ 
+        private void SetCameraImageSource_()
+        {
+            Debug.WriteLine("[SetCameraImageSource_] ");
+
+            //int stride = 320 * 3;
+            Bitmap BackBuffer = new Bitmap(320, 240, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+
+            System.Drawing.Rectangle rect = new System.Drawing.Rectangle(0, 0, 320, 240);
+
+            // get at the bitmap data in a nicer way
+            System.Drawing.Imaging.BitmapData bmpData =
+                BackBuffer.LockBits(rect, System.Drawing.Imaging.ImageLockMode.ReadWrite,
+                BackBuffer.PixelFormat);
+
+            int bufSize = i.bufSize;
+
+            IntPtr ImgPtr = bmpData.Scan0;
+
+            // grab the image
+
+
+            lock (i.thread_locker)
+            {
+                // Copy the RGB values back to the bitmap
+                System.Runtime.InteropServices.Marshal.Copy(i.imageBuffer, 0, ImgPtr, bufSize);
+            }
+            // Unlock the bits.
+            BackBuffer.UnlockBits(bmpData);
+
+            IntPtr hBmp;
+
+            //Convert the bitmap to BitmapSource for use with WPF controls
+            hBmp = BackBuffer.GetHbitmap();
+
+            this.camera_image.Source = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(hBmp, IntPtr.Zero, Int32Rect.Empty, System.Windows.Media.Imaging.BitmapSizeOptions.FromEmptyOptions());
+            this.camera_image.Source.Freeze();
+        }
+
 
     }
 }
