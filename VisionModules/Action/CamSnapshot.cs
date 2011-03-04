d761b580d3edfffea5bb2e94e4d245918fc54292
Sven Kratz <Sven Kratz@localhost>
2011-03-04 12:04:27 +0100

    starting WebcamSnapshot module

---
+++ VisionModules/Action/CamSnapshot.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MayhemCore;
+using MayhemCore.ModuleTypes;
+using System.Runtime.Serialization;
+using System.Windows;
+using VisionModules.Wpf;
+
+namespace VisionModules.Action
+{
+    [Serializable]
+    public class CamSnapshot : ReactionBase, IWpf, ISerializable
+    {
+        protected string folderLocation = "";
+
+        // The device we are recording from
+        
+
+        public CamSnapshot()
+            : base("Webcam Snapshot (OpenCV)", "Takes a photo with your webcam and saves it to the hard drive.")
+        {
+
+
+            Setup();
+        }
+
+        public void Setup()
+        {
+
+            hasConfig = true;
+            // TODO: What if we have multiple of these?
+           
+            SetConfigString();
+        }
+
+        public override void Enable()
+        {
+            base.Enable();
+
+           // TODO: Start Cam
+        }
+
+        public override void Disable()
+        {
+            base.Disable();
+           
+            // TODO: Stop Cam
+        }
+
+        public override void Perform()
+        {
+            // Executes Action
+
+           
+        }
+
+        protected string DateTimeToTimeStamp(DateTime time)
+        {
+            return time.ToString("yyyyMMddHHmmssffff");
+        }
+
+        public void WpfConfig()
+        {
+            /*
+             * Choose a webcam and save folder
+             */
+            var window = new CamSnapshotConfig(folderLocation, null /* cameraDevice */);
+
+            window.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+
+            if (window.ShowDialog() == true)
+            {
+
+                folderLocation = window.location;
+
+                // webcam.Device = window.captureDevice;
+
+                SetConfigString();
+            }
+
+        }
+
+        private void SetConfigString()
+        {
+            ConfigString = String.Format("Save Location: \"{0}\"", folderLocation);
+        }
+
+        #region Serialization
+        public CamSnapshot(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+
+            folderLocation = info.GetString("FolderLocation");
+
+            Setup();
+        }
+
+        public new void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            base.GetObjectData(info, context);
+            info.AddValue("FolderLocation", folderLocation);
+        }
+        #endregion
+    }
+}
---
+++ VisionModules/Wpf/CamSnapshotConfig.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Windows;
+using System.Windows.Forms;
+
+
+namespace VisionModules.Wpf
+{
+	/// <summary>
+	/// Interaction logic for WebcamSnapshotConfig.xaml
+	/// </summary>
+	public partial class CamSnapshotConfig : Window
+	{
+		public string location;
+		// public Device captureDevice;
+
+
+
+		public CamSnapshotConfig(string location, object captureDevice) {
+			this.location = location;
+			
+
+			//InitializeComponent();
+
+            // TODO: Enumerate devices
+	
+
+		}
+
+
+		private void Button_Click_1(object sender, RoutedEventArgs e) {
+			FolderBrowserDialog dlg = new FolderBrowserDialog();
+			dlg.SelectedPath = location;
+
+			if (dlg.ShowDialog() == System.Windows.Forms.DialogResult.OK) {
+				location = dlg.SelectedPath;
+			}
+		}
+
+		private void Button_Save_Click(object sender, RoutedEventArgs e) {
+			// captureDevice = DeviceList.SelectedItem as Device;
+			DialogResult = true;
+		}
+
+		private void Button_Cancel_Click(object sender, RoutedEventArgs e) {
+			DialogResult = false;
+		}
+	}
+}
