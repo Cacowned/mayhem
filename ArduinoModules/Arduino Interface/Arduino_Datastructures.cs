171518246167968c00df22ac3e8b91d25bdb741f
Sven Kratz <Sven Kratz@localhost>
2011-03-26 13:50:33 +0100

    working on Arduino code

---
+++ ArduinoModules/Arduino Interface/Arduino_Datastructures.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ArduinoModules.Arduino_Interface
+{
+    public enum PinMode { INPUT, OUTPUT };
+
+    public struct DigitalPin
+    {
+        public PinMode pin_mode;
+        public bool pin_state;
+
+        public DigitalPin()
+        {
+            pin_mode = PinMode.INPUT;
+            pin_state = false;
+        }
+    }
+
+    public struct AnalogIn
+    {
+        public PinMode pin_mode = PinMode.INPUT;
+        // 16 bit sampling
+        public UInt16 value = 0; 
+ 
+    }
+}
