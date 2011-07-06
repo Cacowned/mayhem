ec6da2d9efb072aabb3bd963ce729f2fa2cc5389
t-elwhi <t-elwhi@ELIWHITE.redmond.corp.microsoft.com>
2011-07-06 12:23:53 -0700

    Added a WP7 interface for modules. Added a WP Class library project that links to the MayhemCore files.

---
+++ MayhemCore/ModuleTypes/IWp7.cs
@@ -0,0 +1,13 @@
+﻿
+namespace MayhemCore.ModuleTypes
+{
+    /// <summary>
+    /// Every module that has configuration
+    /// that wants to be accessible from a Windows Phone 7
+    /// application needs to implement this interface
+    /// </summary>
+    public interface IWp7
+    {
+        void Wp7Config();
+    }
+}
