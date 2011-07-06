533f3713a90b66cc13ad3aedcd7eb5b6a8827953
t-elwhi <t-elwhi@ELIWHITE.redmond.corp.microsoft.com>
2011-07-06 16:35:36 -0700

    Removed extra serializable attributes and removed the missing ModuleTypeResolver class from the core project.

--- MayhemCore/ActionList.cs
+++ MayhemCore/ActionList.cs
@@ -7,7 +7,6 @@ namespace MayhemCore
 	/// Is a list of all the available actions that implement T if they have a configuration window
 	/// </summary>
 	/// <typeparam name="T">The ModuleType interface that every module must implement</typeparam>
-	[Serializable]
 	public class ActionList<T> : ModuleList<ActionBase, T>, IEnumerable<ActionBase>
 	{
 	}
--- MayhemCore/ReactionList.cs
+++ MayhemCore/ReactionList.cs
@@ -6,7 +6,6 @@ namespace MayhemCore
 	/// <summary>
 	/// A list of all the available reactions
 	/// </summary>
-	[Serializable]
 	public class ReactionList<T> : ModuleList<ReactionBase, T>, IEnumerable<ReactionBase>
 	{
 	}
