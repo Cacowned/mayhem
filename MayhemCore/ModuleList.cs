0bc8273f046945baccda0a40a722867dadbd4c71
Jay Meistrich <jme@microsoft.com>
2011-07-08 11:58:56 -0700

    Fixed DataContractSerializer

--- MayhemCore/ModuleList.cs
+++ MayhemCore/ModuleList.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Diagnostics;
 
 namespace MayhemCore
 {
@@ -13,11 +14,14 @@ namespace MayhemCore
 	/// <typeparam name="V">The interface type that modules must implement</typeparam>
 	public abstract class ModuleList<T, V> : List<T> where T : ModuleBase
 	{
-		public ModuleList() {
-            //ScanModules();
+        public List<Type> types;
+		public ModuleList() 
+        {
+            types = new List<Type>();
 		}
 
-        public void ScanModules(string path) {
+        public void ScanModules(string path) 
+        {
             // Load up all the types of things that we want in the application root
             FindTypes(path);
             //Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "modules"));
@@ -30,8 +34,10 @@ namespace MayhemCore
 		/// </summary>
 		/// <param name="path"></param>
 		/// <returns></returns>
-		protected void FindTypes(string path) {
-			if (String.IsNullOrEmpty(path)) {
+		protected void FindTypes(string path)
+        {
+			if (String.IsNullOrEmpty(path))
+            {
 				throw new ArgumentNullException("The given path is null or empty");
 			}
 
@@ -41,27 +47,40 @@ namespace MayhemCore
 			string[] pluginFiles = Directory.GetFiles(path, "*.dll", SearchOption.AllDirectories);
 
 			// Go through each dll file
-			for (int i = 0; i < pluginFiles.Length; i++) {
+			for (int i = 0; i < pluginFiles.Length; i++) 
+            {
 				// load it
-				string path2 = Path.Combine(path, pluginFiles[i]);
-				Assembly ass = Assembly.LoadFrom(path2);
+				string pathFile = Path.Combine(path, pluginFiles[i]);
+                try
+                {
+                    Assembly assembly = Assembly.LoadFrom(pathFile);
 
-				if (ass != null) {
-					// Go through all the public classes in the assembly
-					foreach (var type in ass.GetExportedTypes()) {
-						// If it's parent class is the type we want
-						// and it implements the correct moduleType
-						if (type.IsSubclassOf(typeof(T)) && !type.IsAbstract) {
-							// Create an instance
-							T reaction = (T)Activator.CreateInstance(type);
-							bool hasConfig = reaction.HasConfig;
-							if (!hasConfig || (hasConfig && type.GetInterfaces().Contains(typeof(V)))) {
-								// Add it to our final list
-								this.Add(reaction);
-							}
-						}
-					}
-				}
+                    if (assembly != null)
+                    {
+                        // Go through all the public classes in the assembly
+                        foreach (var type in assembly.GetExportedTypes())
+                        {
+                            // If it's parent class is the type we want
+                            // and it implements the correct moduleType
+                            if (type.IsSubclassOf(typeof(T)) && !type.IsAbstract)
+                            {
+                                // Create an instance
+                                T reaction = (T)Activator.CreateInstance(type);
+                                bool hasConfig = reaction.HasConfig;
+                                if (!hasConfig || (hasConfig && type.GetInterfaces().Contains(typeof(V))))
+                                {
+                                    // Add it to our final list
+                                    this.Add(reaction);
+                                    types.Add(type);
+                                }
+                            }
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Trace.WriteLine("Error opening file: \n" + e);
+                }
 			}
 			this.Sort();
 		}
