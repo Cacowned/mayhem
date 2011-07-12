d2053d39ede66f55e496cdfccd2a744d984e3e8f
Jay Meistrich <jmeistrich@gmail.com>
2011-07-12 10:31:17 -0700

    Fix: connections not enabling on startup. Fix: Utils.SendKey not working on 64-bit

--- DefaultModules/LowLevel/Utils.cs
+++ DefaultModules/LowLevel/Utils.cs
@@ -6,12 +6,14 @@ namespace DefaultModules.LowLevel
 {
 	public static class Utils
 	{
-		public static bool Is64BitProcess {
+		public static bool Is64BitProcess 
+        {
 			get { return IntPtr.Size == 8; }
 		}
 
-		public static void SendKey(ushort key) {
-			if (Is64BitProcess) {
+		public static void SendKey(ushort key, byte scanCode = 0) 
+        {
+			/*if (Is64BitProcess) {
 				Native.INPUT_64[] inputStruct = new Native.INPUT_64[1];
 				inputStruct[0] = new Native.INPUT_64();
 				inputStruct[0].type = Native.INPUT_KEYBOARD;
@@ -33,7 +35,9 @@ namespace DefaultModules.LowLevel
 				// Key up the actual key-code
 				inputStruct[0].ki = Native.createKeybdInput(key, Native.KEYEVENTF_KEYUP);
 				Native.SendInput(1, inputStruct, Marshal.SizeOf(inputStruct[0]));
-			}
+			}*/
+            Native.keybd_event((byte)key, scanCode, 1, UIntPtr.Zero);
+            Native.keybd_event((byte)key, scanCode, 3, UIntPtr.Zero);
 		}
 
 	}
--- DefaultModules/Reactions/Media/PreviousItem.cs
+++ DefaultModules/Reactions/Media/PreviousItem.cs
@@ -8,10 +8,13 @@ namespace DefaultModules.Reactions.Media
 	public class PreviousItem : ReactionBase
 	{
 		public PreviousItem()
-			: base("Media: Previous Item", "Goes to the previous item when triggered") {
+			: base("Media: Previous Item", "Goes to the previous item when triggered") 
+        {
 		}
-		public override void Perform() {
-			Utils.SendKey((ushort)VK.MEDIA_PREV_TRACK);
+
+		public override void Perform()
+        {
+			Utils.SendKey((ushort)VK.MEDIA_PREV_TRACK, 0x22);
 		}
 	}
 }
