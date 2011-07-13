3841056dccf6a611d9bb6c350f32f341a754dfcc
Jay Meistrich <jmeistrich@gmail.com>
2011-07-13 16:30:56 -0700

    Serialize takes stream rather than filename

--- MayhemCore/ConnectionList.cs
+++ MayhemCore/ConnectionList.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.ObjectModel;
 using System.Runtime.Serialization;
+using System.IO;
+using System.Collections.Generic;
+using System.Xml;
 
 namespace MayhemCore
 {
@@ -9,5 +12,27 @@ namespace MayhemCore
 	/// </summary>
 	public class ConnectionList : ObservableCollection<Connection>
 	{
+        public static ConnectionList Deserialize(Stream stream, List<Type> types)
+        {
+            ConnectionList obj = null;
+            DataContractSerializer dcs = new DataContractSerializer(typeof(ConnectionList));
+
+            using (XmlDictionaryReader reader = XmlDictionaryReader.CreateTextReader(stream, XmlDictionaryReaderQuotas.Max))
+            {
+                obj = dcs.ReadObject(reader, false, new ModuleTypeResolver(types)) as ConnectionList;
+            }
+
+            return obj;
+        }
+
+        public void Serialize(Stream stream)
+        {
+            DataContractSerializer dcs = new DataContractSerializer(typeof(ConnectionList));
+
+            using (XmlDictionaryWriter writer = XmlDictionaryWriter.CreateTextWriter(stream))
+            {
+                dcs.WriteObject(writer, this, new ModuleTypeResolver());
+            }
+        }
 	}
 }
