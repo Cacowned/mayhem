53fd032e3cf9a5aaaaeb470c42ed6dfd47b59cd0
Eli <Eli@Eli-Desktop.Home>
2011-09-01 17:10:32 -0700

    Making the MayhemWpf application use NuGet.
    References to MayhemCore and MayhemDefaultStyles are now dependent on debug / release mode. Debug uses project references. Release uses package references.

---
+++ MayhemCore/ConnectionList.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Runtime.Serialization;
+using System.IO;
+using System.Collections.Generic;
+using System.Xml;
+
+namespace MayhemCore
+{
+	/// <summary>
+	/// Maintains a set of all the current connections
+	/// </summary>
+	public class ConnectionList : ObservableCollection<Connection>
+	{
+        public static ConnectionList Deserialize(Stream stream, List<Type> types)
+        {
+            ConnectionList obj = null;
+            DataContractSerializer dcs = new DataContractSerializer(typeof(ConnectionList));
+
+            using (XmlDictionaryReader reader = XmlDictionaryReader.CreateTextReader(stream, XmlDictionaryReaderQuotas.Max))
+            {
+                obj = dcs.ReadObject(reader, false, new ModuleTypeResolver(types)) as ConnectionList;
+            }
+
+            return obj;
+        }
+
+        public void Serialize(Stream stream)
+        {
+            DataContractSerializer dcs = new DataContractSerializer(typeof(ConnectionList));
+
+            using (XmlDictionaryWriter writer = XmlDictionaryWriter.CreateTextWriter(stream))
+            {
+                dcs.WriteObject(writer, this, new ModuleTypeResolver());
+            }
+        }
+	}
+}
---
+++ MayhemWpf/App.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using NuGet;
+using System.IO;
+
+
+namespace MayhemWpf
+{
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : Application
+	{
+		private void Application_Startup(object sender, StartupEventArgs e)
+		{
+            MainWindow main = new MainWindow();
+            Application.Current.ShutdownMode = ShutdownMode.OnMainWindowClose;
+            Application.Current.MainWindow = main;
+			
+            if (e.Args.Any())
+			{
+			    string packageFile = e.Args[0];
+			    var zipFile = new ZipPackage(packageFile);
+
+			    InstallModule window = new InstallModule(zipFile);
+
+                if (window.ShowDialog() == true)
+                {
+                    // Installation was a success
+                }
+                else
+                {
+                    // Installation failed?
+                }
+
+                // Close the application 
+                this.Shutdown();
+            }
+
+            main.Load();
+            main.Show();
+
+		}
+	}
+}
