6dfc1bd58fd886f9dfcb4ca4ee8746defc50f11f
SvenKratz <SvenKratz@localhost>
2011-09-13 15:52:51 -0700

    more arduino-related implementation work

---
+++ Modules/ArduinoModules/Wpf/Helpers/DigitalPinWriteItem.cs
@@ -0,0 +1,48 @@
+﻿/*
+ * DitialPinItem.cs
+ * 
+ * 
+ * Data model for Gridviews on Arduino digital pins for ArduinoDigitalWriteConfig
+ * 
+ * 
+ * (c) 2011, Microsoft Applied Sciences Group
+ * 
+ * Author: Sven Kratz
+ * 
+ * 
+ */ 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ArduinoModules.Firmata;
+
+namespace ArduinoModules.Wpf.Helpers
+{
+    public class DigitalPinWriteItem
+    {
+        private bool active_;
+        public bool Active { get; set; }
+
+        private int firmata_id=0;
+
+        private DIGITAL_WRITE_MODE write_mode_; 
+        public DIGITAL_WRITE_MODE WriteMode
+        {
+            get { return write_mode_; }
+            set { write_mode_ = value; }
+        }
+        
+        public string PinName
+        {
+            get { return "D" + firmata_id; }
+        }
+
+        public DigitalPinWriteItem(bool check, int id, DIGITAL_WRITE_MODE mode)
+        {
+            Active = check;
+            firmata_id = id;
+            WriteMode = mode; 
+        }
+    }
+}
