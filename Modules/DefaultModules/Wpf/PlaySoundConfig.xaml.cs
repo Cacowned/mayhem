4ef41cf22ed0251388bf60983d1fc0e688cecc95
SvenKratz <SvenKratz@localhost>
2011-09-15 17:12:49 -0700

    fixed audio config

--- Modules/DefaultModules/Wpf/PlaySoundConfig.xaml.cs
+++ Modules/DefaultModules/Wpf/PlaySoundConfig.xaml.cs
@@ -42,6 +42,7 @@ namespace DefaultModules.Wpf
             if (dlg.ShowDialog() == true)
             {
                 FileName = dlg.FileName;
+                LocationBox.Text = FileName;
             }
         }
 
