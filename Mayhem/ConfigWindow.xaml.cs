f6fba4e260270e9117d1ee61e00a36b22443673b
SvenKratz <SvenKratz@localhost>
2011-09-20 18:20:01 -0700

    Changes to Camera and ImagerBase that fix an inheritance issue.

---
+++ Mayhem/ConfigWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System.Threading;
+using System.Windows;
+using MayhemCore;
+using MayhemCore.ModuleTypes;
+using MayhemWpf.UserControls;
+using System.Windows.Threading;
+
+namespace Mayhem
+{
+    /// <summary>
+    /// Interaction logic for ConfigWindow.xaml
+    /// </summary>
+    public partial class ConfigWindow : Window
+    {
+        IWpfConfigurable iWpf;
+        IWpfConfiguration iWpfConfig;
+
+        public ConfigWindow(IWpfConfigurable iWpf)
+        {
+            this.Owner = Application.Current.MainWindow;
+            this.iWpf = iWpf;
+            this.iWpfConfig = (IWpfConfiguration)iWpf.ConfigurationControl;
+            InitializeComponent();
+            ConfigContent.Content = iWpfConfig;
+
+            buttonSave.IsEnabled = iWpfConfig.CanSave;
+
+            windowHeader.Text = "Config: " + iWpfConfig.Title;
+            iWpfConfig.CanSavedChanged += new IWpfConfiguration.ConfigCanSaveHandler(iWpfConfig_CanSavedChanged);
+        }
+
+        void iWpfConfig_CanSavedChanged(bool canSave)
+        {
+            this.Dispatcher.Invoke(DispatcherPriority.Normal, (System.Action)(() =>
+            {
+                buttonSave.IsEnabled = canSave;
+            }));
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            iWpfConfig.OnLoad();
+        }
+
+        private void Button_Save_Click(object sender, RoutedEventArgs e)
+        {
+            IWpfConfiguration config = ConfigContent.Content as IWpfConfiguration;
+            iWpfConfig.OnSave();
+            iWpf.OnSaved(config);
+            ((ModuleBase)iWpf).SetConfigString();
+
+            ThreadPool.QueueUserWorkItem(new WaitCallback((o) =>
+            {
+                iWpfConfig.OnClosing();
+            }));
+            ((MainWindow)Application.Current.MainWindow).Save();
+            DialogResult = true;
+        }
+
+        private void Button_Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            ThreadPool.QueueUserWorkItem(new WaitCallback((o) =>
+            {
+                iWpfConfig.OnCancel();
+                iWpfConfig.OnClosing();
+            }));
+            DialogResult = false;
+        }
+    }
+}
