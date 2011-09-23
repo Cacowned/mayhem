8d5722424160628a8898b2d69d08202bae8fd59a
Eli <Eli@Eli-Desktop.Home>
2011-09-23 14:12:45 -0700

    Updated the phidget configuration dialogs. They now share some validation and error messages.

--- Modules/PhidgetModules/Wpf/UserControls/PhidgetConfigControl.cs
+++ Modules/PhidgetModules/Wpf/UserControls/PhidgetConfigControl.cs
@@ -18,5 +18,18 @@ namespace PhidgetModules.Wpf.UserControls
 
         public virtual void OnLoad() { }
         public virtual void OnSave() { }
+
+        public virtual string CheckValidity() { return ""; }
+
+        public delegate void Revalidate();
+        public event Revalidate OnRevalidate;
+
+        protected void Validate()
+        {
+            if (OnRevalidate != null)
+            {
+                OnRevalidate();
+            }
+        }
     }
 }
