3fd7fdbed8742729aaa9763b04edcfeff8aa62e4
jmeistrich <jmeistrich@gmail.com>
2011-09-25 20:51:05 -0700

    WindowSequence ConfigString

--- Modules/WindowModules/WindowSequence.cs
+++ Modules/WindowModules/WindowSequence.cs
@@ -50,88 +50,23 @@ namespace WindowModules
         {
             WindowSequenceConfig config = (WindowSequenceConfig)configurationControl;
             actionInfo = config.ActionInfo;
-            ConfigString = "";
         }
 
-
-
-        //bool Report(IntPtr hwnd, int lParam)
-        //{
-        //    if (IsTaskbarWindow(hwnd.ToInt32()))
-        //    {
-        //        int procid = 0;
-        //        int threadid = Native.GetWindowThreadProcessId(hwnd, ref procid);
-        //        try
-        //        {
-        //            Process p = Process.GetProcessById(procid);
-        //            bool isMatch = true;
-        //            string filename;
-        //            try
-        //            {
-        //                filename = p.MainModule.FileName;
-        //            }
-        //            catch
-        //            {
-        //                filename = WMIProcess.GetFilename(procid);
-        //            }
-        //            if (filename != null)
-        //            {
-        //                FileInfo fi = new FileInfo(filename);
-        //                filename = fi.Name;
-        //            }
-        //            if (actionInfo.WindowInfo.CheckFileName && !filename.ToLower().EndsWith(actionInfo.WindowInfo.FileName.ToLower()))
-        //                isMatch = false;
-        //            if (isMatch)
-        //            {
-        //                if (actionInfo.WindowInfo.CheckTitle)
-        //                {
-        //                    StringBuilder sb = new StringBuilder(200);
-        //                    Native.GetWindowText(hwnd, sb, sb.Capacity);
-        //                    string title = sb.ToString();
-        //                    if (title != actionInfo.WindowInfo.Title)
-        //                        isMatch = false;
-        //                }
-        //                if (isMatch)
-        //                {
-        //                    Logger.WriteLine("Found: " + hwnd);
-        //                    foreach (WindowAction action in actionInfo.WindowActions)
-        //                    {
-        //                        action.Perform(hwnd);
-        //                        Thread.Sleep(50);
-        //                    }
-        //                    return false;
-        //                }
-        //            }
-        //        }
-        //        catch { }
-        //    }
-        //    return true;
-        //}
-
-        //void FindWindowAndPerform(string className, IntPtr prevWindow)
-        //{
-        //    IntPtr hwnd = Native.FindWindowEx(IntPtr.Zero, prevWindow, className, actionInfo.WindowInfo.Title);
-        //    Report(hwnd, 0);
-        //}
+        public override void SetConfigString()
+        {
+            ConfigString = actionInfo.WindowInfo.Title;
+        }
 
         public override void Perform()
         {
-            //if (actionInfo.WindowInfo.CheckTitle)
-            //{
-            //    FindWindowAndPerform(null, IntPtr.Zero);
-            //}
-            //else
-            //{
-                //Native.EnumWindows(new Native.EnumWindowsCallback(Report), 0);
-            //}
-                WindowFinder.Find(actionInfo, new WindowFinder.WindowActionResult((hwnd) =>
+            WindowFinder.Find(actionInfo, new WindowFinder.WindowActionResult((hwnd) =>
+                {
+                    foreach (WindowAction action in actionInfo.WindowActions)
                     {
-                        foreach (WindowAction action in actionInfo.WindowActions)
-                        {
-                            action.Perform(hwnd);
-                            Thread.Sleep(50);
-                        }
-                    }));
+                        action.Perform(hwnd);
+                        Thread.Sleep(50);
+                    }
+                }));
         }
     }
 }
