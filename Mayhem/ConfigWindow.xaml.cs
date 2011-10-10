032d2c88f14135238ebd1b02cb56cc0f6ca0e103
jmeistrich <jmeistrich@gmail.com>
2011-10-10 00:19:14 -0700

    Fixed #95: Config window not resizing

--- Mayhem/ConfigWindow.xaml.cs
+++ Mayhem/ConfigWindow.xaml.cs
@@ -1,5 +1,10 @@
-﻿using System.Threading;
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+using System.Threading;
 using System.Windows;
+using System.Windows.Interop;
+using System.Windows.Media.Animation;
 using System.Windows.Threading;
 using MayhemCore;
 using MayhemWpf.ModuleTypes;
@@ -14,6 +19,9 @@ namespace Mayhem
     {
         IWpfConfigurable iWpf;
         WpfConfiguration iWpfConfig;
+        const double AnimationTime = 0.2;
+
+        Size previousSize;
 
         public ConfigWindow(IWpfConfigurable iWpf)
         {
@@ -27,6 +35,36 @@ namespace Mayhem
 
             windowHeader.Text = iWpfConfig.Title;
             iWpfConfig.CanSavedChanged += iWpfConfig_CanSavedChanged;
+
+            iWpfConfig.Loaded += new RoutedEventHandler(iWpfConfig_Loaded);
+
+            // In constructor subscribe to the Change event of the WindowRect DependencyProperty
+            DependencyPropertyDescriptor dpd = DependencyPropertyDescriptor.FromProperty(WindowRectProperty, typeof(ConfigWindow));
+            if (dpd != null)
+            {
+                dpd.AddValueChanged(this, delegate
+                {
+                    ResizeWindow(WindowRect);
+                });
+            }
+        }
+
+        void iWpfConfig_Loaded(object sender, RoutedEventArgs e)
+        {
+            previousSize = new Size(ActualWidth, ActualHeight);
+            iWpfConfig.SizeChanged += new SizeChangedEventHandler(ConfigContent_SizeChanged);
+        }
+
+        private void ConfigContent_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            WindowRect = new Rect(Left, Top, ActualWidth, ActualHeight);
+            double targetWidth = iWpfConfig.Width + 40;
+
+            Rect target = new Rect(Left - (ActualWidth - previousSize.Width) / 2, Top - (ActualHeight - previousSize.Height) / 2,
+                                   ActualWidth, ActualHeight);
+
+            previousSize = new Size(ActualWidth, ActualHeight);
+            StartStoryBoard(WindowRect, target, AnimationTime);
         }
 
         void iWpfConfig_CanSavedChanged(bool canSave)
@@ -94,5 +132,55 @@ namespace Mayhem
             });
             DialogResult = false;
         }
+
+        #region Window Resizing
+        public Rect WindowRect
+        {
+            get
+            {
+                return (Rect)GetValue(WindowRectProperty);
+            }
+            set
+            {
+                SetValue(WindowRectProperty, value);
+            }
+        }
+
+        // Using a DependencyProperty as the backing store for WindowRect.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty WindowRectProperty =
+            DependencyProperty.Register("WindowRect", typeof(Rect), typeof(ConfigWindow), new UIPropertyMetadata(new Rect(0, 0, 0, 0)));
+
+        [DllImport("user32.dll", SetLastError = true)]
+        internal static extern bool MoveWindow(IntPtr hWnd, int X, int Y, int nWidth, int nHeight, bool bRepaint);
+
+        /// <summary>
+        /// Resizes the window to the desired Rect
+        /// Called when WindowRect DependencyProperty changes
+        /// </summary>
+        /// <param name="value">The target Rect containing size and Position</param>
+        private void ResizeWindow(Rect value)
+        {
+            IntPtr windowPtr = new WindowInteropHelper(this).Handle;
+            MoveWindow(windowPtr, (int)value.Left, (int)value.Top, (int)value.Width, (int)value.Height, true);
+        }
+
+        private void StartStoryBoard(Rect currentRect, Rect targetRect, double time)
+        {
+            RectAnimation rectAnimation = new RectAnimation();
+            rectAnimation.Duration = TimeSpan.FromSeconds(time);
+            rectAnimation.FillBehavior = FillBehavior.HoldEnd;
+
+            rectAnimation.To = targetRect;
+
+            Storyboard.SetTarget(rectAnimation, this);
+            Storyboard.SetTargetProperty(rectAnimation, new PropertyPath(WindowRectProperty));
+
+            Storyboard storyBoard = new Storyboard();
+            storyBoard.Children.Add(rectAnimation);
+
+            storyBoard.Begin(this);
+        }
+
+        #endregion
     }
 }
