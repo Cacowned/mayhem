0330557e8420e58594e6d7c16fa5a421ab3642dd
SvenKratz <SvenKratz@localhost>
2011-10-12 17:56:06 -0700

    Mayduino Event

---
+++ Modules/ArduinoModules/Wpf/MayduinoEventConfig.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MayhemWpf.UserControls;
+using MayhemSerial;
+
+namespace ArduinoModules.Wpf
+{
+    /// <summary>
+    /// Interaction logic for MayduinoEvent.xaml
+    /// </summary>
+
+    public partial class MayduinoEventConfig : WpfConfiguration
+    {
+        private MayhemSerialPortMgr serial = MayhemSerialPortMgr.Instance;
+        private Dictionary<string, string> deviceNamesIds = null;
+
+
+        public string ArduinoPortName
+        {
+            get;
+            private set;
+        }
+
+        public MayduinoEventConfig()
+        {
+            InitializeComponent();
+        }
+
+        public override void OnLoad()
+        {
+           
+            serial.UpdatePortList();
+            deviceNamesIds = serial.GetArduinoPortNames();
+
+            if (deviceNamesIds.Count > 0)
+            {
+                deviceList.ItemsSource = deviceNamesIds;
+                deviceList.DisplayMemberPath = "Value";
+                deviceList.SelectedValuePath = "Key";
+                deviceList.SelectedIndex = 0;
+            }
+
+            // directly display the list if there is only one board attached
+            if (deviceNamesIds.Count == 1)
+            {
+                connectButton_Click(this, null);
+            }
+            CanSave = true;
+            FillSelections();
+        }
+
+        private void connectButton_Click(object sender, RoutedEventArgs e)
+        {
+            ArduinoPortName  = (string)deviceList.SelectedValue;              
+        }
+
+        public virtual void FillSelections()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void deviceList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ArduinoPortName = (string)deviceList.SelectedValue;    
+        }
+    }
+}
