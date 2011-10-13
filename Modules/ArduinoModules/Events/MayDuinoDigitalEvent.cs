43d572f64411f90681460452bd18b97e1f1a53cd
SvenKratz <SvenKratz@localhost>
2011-10-12 17:56:06 -0700

    Mayduino Event

---
+++ Modules/ArduinoModules/Events/MayDuinoDigitalEvent.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MayhemCore;
+using MayhemWpf.ModuleTypes;
+using System.Runtime.Serialization;
+using ArduinoModules.Wpf;
+
+namespace ArduinoModules.Events
+{
+    [DataContract]
+    [MayhemModule("MayDuino Digital Event", "Event Setup for MayDuino Core")]
+    public class MayDuinoDigitalEvent : EventBase, IWpfConfigurable
+    {
+        protected override void OnEnabling(EnablingEventArgs e)
+        {
+         
+        }
+
+        public MayhemWpf.UserControls.WpfConfiguration ConfigurationControl
+        {
+            get 
+            {
+                return new MayDuinoDigitalEventConfig();
+            }
+        }
+
+        public void OnSaved(MayhemWpf.UserControls.WpfConfiguration configurationControl)
+        {
+           // throw new NotImplementedException();
+        }
+
+        public string GetConfigString()
+        {
+            //throw new NotImplementedException();
+            return "MayDuino Digital";
+        }
+    }
+}
---
+++ Modules/ArduinoModules/Wpf/MayDuinoDigitalEventConfig.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ArduinoModules.Wpf
+{
+    public class MayDuinoDigitalEventConfig : MayduinoEventConfig
+    {
+        public override void FillSelections()
+        {
+            // hide activation value
+            lbl_eventVals.Visibility = System.Windows.Visibility.Collapsed;
+            bx_eventVals.Visibility = System.Windows.Visibility.Collapsed;
+
+            // fill pin values
+            for (int i = 2; i <= 12; i++)
+            {
+                string pinString = "D" + i;
+                bx_pin.Items.Add(pinString);
+            }
+
+            // fill event types
+            bx_cond.Items.Add("HIGH");
+            bx_cond.Items.Add("LOW");
+
+            // fill
+        }
+
+        public override string Title
+        {
+            get
+            {
+                return "MayDuino Digital Event";
+            }
+        }
+    }
+}
