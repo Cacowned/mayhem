a71fb7f51fb835ebea32c3f67bf5a2aee65cdb40
Eli <Eli@Eli-Desktop.Home>
2011-10-14 01:21:24 -0700

    Added Open Website as a reaction inside of Default Modules.

---
+++ Modules/DefaultModules/Wpf/OpenUrlConfig.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using MayhemWpf.UserControls;
+using Microsoft.Win32;
+using System.Net;
+using System.Net.Sockets;
+
+namespace DefaultModules.Wpf
+{
+    public partial class OpenUrlConfig : WpfConfiguration
+    {
+        public string Url
+        {
+            get;
+            private set;
+        }
+
+        public OpenUrlConfig(string url)
+        {
+            this.Url = url;
+
+            InitializeComponent();
+        }
+
+        public override string Title
+        {
+            get { return "Open Website"; }
+        }
+
+        public override void OnLoad()
+        {
+            UrlBox.Text = Url;
+        }
+
+        public override void OnSave()
+        {
+            Url = UrlBox.Text;
+        }
+
+        private void CheckValidity()
+        {
+            CanSave = UrlBox.Text.Length > 0;
+        }
+        
+        private void UrlBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            CheckValidity();
+
+            textInvalid.Visibility = CanSave ? Visibility.Collapsed : Visibility.Visible;
+        }
+    }
+}
