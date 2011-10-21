1d54171d9598889ad4016978a9c8cb074a34bfd7
SvenKratz <SvenKratz@localhost>
2011-10-21 16:34:23 -0700

    Kinect event and config

---
+++ Modules/KinectModules/Events/KinectSkeletalGesture.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MayhemCore;
+using MayhemWpf.ModuleTypes;
+using KinectModules.Wpf;
+using System.Runtime.Serialization;
+using Microsoft.Research.Kinect.Nui;
+using System.Windows.Media;
+
+namespace KinectModules
+{
+    [DataContract]
+    [MayhemModule("HTTP Image Server", "Runs a simple http server and updates the image when triggered")]
+    public class KinectSkeletalGesture : EventBase, IWpfConfigurable
+    {
+
+        public MayhemWpf.UserControls.WpfConfiguration ConfigurationControl
+        {
+            get 
+            {
+                return new KinectEventConfig();
+            }
+        }
+
+        public void OnSaved(MayhemWpf.UserControls.WpfConfiguration configurationControl)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetConfigString()
+        {
+            throw new NotImplementedException();
+        }
+
+        
+
+    }
+}
