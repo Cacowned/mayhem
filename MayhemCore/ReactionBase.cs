e1e8b3a1436dde75e8783cdf3996ad190c736164
v-elwhi <v-elwhi@ASGW510.redmond.corp.microsoft.com>
2011-10-21 13:48:45 -0700

    Added comments to public and protected methods in MayhemCore part 1

--- MayhemCore/ReactionBase.cs
+++ MayhemCore/ReactionBase.cs
@@ -2,9 +2,16 @@
 
 namespace MayhemCore
 {
+    /// <summary>
+    /// The base class for all Mayhem reactions
+    /// </summary>
     [DataContract]
     public abstract class ReactionBase : ModuleBase
     {
+        /// <summary>
+        /// This method is called when the connection is triggered. The reaction
+        /// should take action here.
+        /// </summary>
         public abstract void Perform();
     }
 }
