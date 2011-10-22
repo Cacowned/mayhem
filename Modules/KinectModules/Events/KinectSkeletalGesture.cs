d85755a648cf521a489d5b3c297ae84d5b7edda7
SvenKratz <SvenKratz@localhost>
2011-10-21 18:33:10 -0700

    Added referenced DLLs from Kinect SDK

--- Modules/KinectModules/Events/KinectSkeletalGesture.cs
+++ Modules/KinectModules/Events/KinectSkeletalGesture.cs
@@ -8,6 +8,8 @@ using KinectModules.Wpf;
 using System.Runtime.Serialization;
 using Microsoft.Research.Kinect.Nui;
 using System.Windows.Media;
+using DTWGestureRecognition;
+using System.Collections;
 
 namespace KinectModules
 {
@@ -15,6 +17,13 @@ namespace KinectModules
     [MayhemModule("Kinect Event", "Triggers when a Kinect gesture is detected")]
     public class KinectSkeletalGesture : EventBase, IWpfConfigurable
     {
+
+        protected override void OnEnabling(EnablingEventArgs e)
+        {
+            base.OnEnabling(e);
+        }
+
+
 
         public MayhemWpf.UserControls.WpfConfiguration ConfigurationControl
         {
@@ -32,6 +41,87 @@ namespace KinectModules
         public string GetConfigString()
         {
             throw new NotImplementedException();
+        }
+
+        private ArrayList _video;
+        private DtwGestureRecognizer _dtw;
+
+        protected override void OnAfterLoad()
+        {
+            _video = new ArrayList();
+            _dtw = new DtwGestureRecognizer(12, 0.6, 2, 2, 10);
+            _flipFlop = 0; 
+        }
+
+        /// <summary>
+        /// The minumum number of frames in the _video buffer before we attempt to start matching gestures
+        /// </summary>
+        private const int MinimumFrames = 6;
+
+        /// <summary>
+        /// How many skeleton frames to store in the _video buffer
+        /// </summary>
+        private const int BufferSize = 32;
+
+        /// <summary>
+        /// How many skeleton frames to ignore (_flipFlop)
+        /// 1 = capture every frame, 2 = capture every second frame etc.
+        /// </summary>
+        private const int Ignore = 2;
+
+        /// <summary>
+        /// Switch used to ignore certain skeleton frames
+        /// </summary>
+        private int _flipFlop;
+
+        private Runtime nui; 
+
+        /// <summary>
+        /// Runs every time our 2D coordinates are ready.
+        /// </summary>
+        /// <param name="sender">The sender object</param>
+        /// <param name="a">Skeleton 2Ddata Coord Event Args</param>
+        private void NuiSkeleton2DdataCoordReady(object sender, Skeleton2DdataCoordEventArgs a)
+        {
+           
+
+            // We need a sensible number of frames before we start attempting to match gestures against remembered sequences
+            if (_video.Count > MinimumFrames )
+            {
+                ////Debug.WriteLine("Reading and video.Count=" + video.Count);
+                string s = _dtw.Recognize(_video);
+                //results.Text = "Recognised as: " + s;
+                if (!s.Contains("__UNKNOWN"))
+                {
+                    // There was no match so reset the buffer
+                    _video = new ArrayList();
+                }
+            }
+
+            // Ensures that we remember only the last x frames
+            if (_video.Count > BufferSize)
+            {
+                
+                // Remove the first frame in the buffer
+                _video.RemoveAt(0);
+                
+            }
+
+            // Decide which skeleton frames to capture. Only do so if the frames actually returned a number. 
+            // For some reason my Kinect/PC setup didn't always return a double in range (i.e. infinity) even when standing completely within the frame.
+            // TODO Weird. Need to investigate this
+            if (!double.IsNaN(a.GetPoint(0).X))
+            {
+                // Optionally register only 1 frame out of every n
+                _flipFlop = (_flipFlop + 1) % Ignore;
+                if (_flipFlop == 0)
+                {
+                _video.Add(a.GetCoords());
+                }
+            }
+
+            // Update the debug window with Sequences information
+            //dtwTextOutput.Text = _dtw.RetrieveText();
         }
 
         
