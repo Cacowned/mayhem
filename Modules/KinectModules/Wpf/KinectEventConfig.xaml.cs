fadd20fe295dc06372f4cadba75fa64382e5802d
SvenKratz <SvenKratz@localhost>
2011-10-23 19:06:36 -0700

    Adding feedback about the gesture recognition to the config.

--- Modules/KinectModules/Wpf/KinectEventConfig.xaml.cs
+++ Modules/KinectModules/Wpf/KinectEventConfig.xaml.cs
@@ -14,6 +14,9 @@ using System.Windows.Shapes;
 using MayhemWpf.UserControls;
 using Microsoft.Research.Kinect.Nui;
 using KinectModules.Helpers;
+using System.Collections;
+using DTWGestureRecognition;
+using MayhemCore;
 
 namespace KinectModules.Wpf
 {
@@ -22,10 +25,37 @@ namespace KinectModules.Wpf
     /// </summary>
     public partial class KinectEventConfig : WpfConfiguration
     {
-       // private Runtime nui;
-        private MayhemKinect kinect;
 
-        private EventHandler<SkeletonFrameReadyEventArgs> skeletonFrameHandler; 
+        /// <summary>
+        /// The minumum number of frames in the _video buffer before we attempt to start matching gestures
+        /// </summary>
+        private const int MinimumFrames = 6;
+
+        /// <summary>
+        /// How many skeleton frames to store in the _video buffer
+        /// </summary>
+        private const int BufferSize = 32;
+
+        /// <summary>
+        /// How many skeleton frames to ignore (_flipFlop)
+        /// 1 = capture every frame, 2 = capture every second frame etc.
+        /// </summary>
+        private const int Ignore = 2;
+
+        /// <summary>
+        /// Switch used to ignore certain skeleton frames
+        /// </summary>
+        private int _flipFlop;
+
+        private ArrayList _video;
+        private DtwGestureRecognizer _dtw;
+
+        // private Runtime nui;
+        private MayhemKinect kinect;
+        // handler for rendering of skeleton frames
+        private EventHandler<SkeletonFrameReadyEventArgs> skeletonRenderFrameHandler;
+        // handler for recognition of skeleton gestures
+        private EventHandler<SkeletonFrameReadyEventArgs> skeletonRecogFrameHandler; 
 
         Dictionary<JointID, Brush> jointColors = new Dictionary<JointID, Brush>() { 
             {JointID.HipCenter, new SolidColorBrush(Color.FromRgb(169, 176, 155))},
@@ -57,40 +87,30 @@ namespace KinectModules.Wpf
 
         public override void OnLoad()
         {
-            
-           // nui = new Runtime();
-
-           // try
-           // {
-           //     nui.Initialize(RuntimeOptions.UseDepthAndPlayerIndex | RuntimeOptions.UseSkeletalTracking | RuntimeOptions.UseColor);
-           // }
-           // catch (InvalidOperationException)
-           // {
-           //     System.Windows.MessageBox.Show("Runtime initialization failed. Please make sure Kinect device is plugged in.");
-           //     return;
-           // }
-
-
-           // try
-           // {
-           //    // nui.VideoStream.Open(ImageStreamType.Video, 2, ImageResolution.Resolution640x480, ImageType.Color);
-           //   //  nui.DepthStream.Open(ImageStreamType.Depth, 2, ImageResolution.Resolution320x240, ImageType.DepthAndPlayerIndex);
-           // }
-           // catch (InvalidOperationException)
-           // {
-           //     System.Windows.MessageBox.Show("Failed to open stream. Please make sure to specify a supported image type and resolution.");
-           //     return;
-           // }
-
-           
-
-           // //nui.DepthFrameReady += new EventHandler<ImageFrameReadyEventArgs>(nui_DepthFrameReady);
-           // nui.SkeletonFrameReady += new EventHandler<SkeletonFrameReadyEventArgs>(nui_SkeletonFrameReady);
-           //// nui.VideoFrameReady += new EventHandler<ImageFrameReadyEventArgs>(nui_ColorFrameReady);
 
             kinect = MayhemKinect.Instance;
-            skeletonFrameHandler = new EventHandler<SkeletonFrameReadyEventArgs>(nui_SkeletonFrameReady);
-            kinect.AttachSkeletonEventHandler(skeletonFrameHandler);
+            skeletonRenderFrameHandler = new EventHandler<SkeletonFrameReadyEventArgs>(nui_SkeletonFrameReady);
+            kinect.AttachSkeletonEventHandler(skeletonRenderFrameHandler);
+
+            skeletonRecogFrameHandler = new EventHandler<SkeletonFrameReadyEventArgs>(SkeletonExtractSkeletonFrameReady);
+            kinect.AttachSkeletonEventHandler(skeletonRecogFrameHandler);
+
+            _video = new ArrayList();
+            _dtw = new DtwGestureRecognizer(12, 0.6, 2, 2, 10);
+            _flipFlop = 0;
+
+            kinect = MayhemKinect.Instance;
+            skeletonRenderFrameHandler = new EventHandler<SkeletonFrameReadyEventArgs>(SkeletonExtractSkeletonFrameReady);
+
+            // skeleton data extractor
+            Skeleton2DDataExtract.Skeleton2DdataCoordReady += NuiSkeleton2DdataCoordReady;
+
+            // read gesture file
+            if (_dtw.LoadGesturesFromFile(KinectSkeletalGesture.GestureFileLocation)) { }
+            else
+            {
+                ErrorLog.AddError(ErrorType.Failure, "Could not read gesture definition file!");
+            }
             
 
         }
@@ -167,9 +187,75 @@ namespace KinectModules.Wpf
             } // for each skeleton
         }
 
+        /// <summary>
+        /// The only thing that needs to be done here is to detach the skeleton event handlers
+        /// </summary>
         public override void  OnClosing()
         {
-            kinect.DetachSkeletonEventHandler(skeletonFrameHandler);      
+            kinect.DetachSkeletonEventHandler(skeletonRenderFrameHandler);
+            kinect.DetachSkeletonEventHandler(skeletonRecogFrameHandler);
+        }
+
+        /// <summary>
+        /// Called each time a skeleton frame is ready. Passes skeletal data to the DTW processor
+        /// </summary>
+        /// <param name="sender">The sender object</param>
+        /// <param name="e">Skeleton Frame Ready Event Args</param>
+        private static void SkeletonExtractSkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
+        {
+            SkeletonFrame skeletonFrame = e.SkeletonFrame;
+            foreach (SkeletonData data in skeletonFrame.Skeletons)
+            {
+                Skeleton2DDataExtract.ProcessData(data);
+            }
+        }
+
+        /// <summary>
+        /// Runs every time our 2D coordinates are ready.
+        /// </summary>
+        /// <param name="sender">The sender object</param>
+        /// <param name="a">Skeleton 2Ddata Coord Event Args</param>
+        private void NuiSkeleton2DdataCoordReady(object sender, Skeleton2DdataCoordEventArgs a)
+        {
+
+
+            // We need a sensible number of frames before we start attempting to match gestures against remembered sequences
+            if (_video.Count > MinimumFrames)
+            {
+                ////Debug.WriteLine("Reading and video.Count=" + video.Count);
+                string s = _dtw.Recognize(_video);
+                //results.Text = "Recognised as: " + s;
+                if (!s.Contains("__UNKNOWN"))
+                {
+                    // There was no match so reset the buffer
+                    _video = new ArrayList();
+                }
+            }
+
+            // Ensures that we remember only the last x frames
+            if (_video.Count > BufferSize)
+            {
+
+                // Remove the first frame in the buffer
+                _video.RemoveAt(0);
+
+            }
+
+            // Decide which skeleton frames to capture. Only do so if the frames actually returned a number. 
+            // For some reason my Kinect/PC setup didn't always return a double in range (i.e. infinity) even when standing completely within the frame.
+            // TODO Weird. Need to investigate this
+            if (!double.IsNaN(a.GetPoint(0).X))
+            {
+                // Optionally register only 1 frame out of every n
+                _flipFlop = (_flipFlop + 1) % Ignore;
+                if (_flipFlop == 0)
+                {
+                    _video.Add(a.GetCoords());
+                }
+            }
+
+            // Update the debug window with Sequences information
+            //dtwTextOutput.Text = _dtw.RetrieveText();
         }
         
     }
