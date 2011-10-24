b64864fcf9b42dcae3451552ea3f5b98502f49b3
SvenKratz <SvenKratz@localhost>
2011-10-24 14:01:05 -0700

    added Mayduino-Core Arduino project.

---
+++ Modules/ArduinoModules/MayDuino/Mayduino-Core/Config.cpp
@@ -0,0 +1,24 @@
+#include "Config.h"
+
+
+Config::Config() : 
+  eventType(EVENT_DISABLED),
+  eventCondition(0xff),
+  eventArgs(0xffff),
+  reactionType(REACTION_DISABLED),
+  reactionCondition(0xff),
+  reactionArgs(0xffff)
+{}
+
+Config::Config(EventType eType, byte eCondition, int eArgs, ReactionType rType, byte rCondition,  int rArgs) :
+  eventType(eType),
+  eventCondition(eCondition),
+  eventArgs(eArgs),
+  reactionType(rType),
+  reactionCondition(rCondition),
+  reactionArgs(rArgs)
+{
+}
+
+
+
