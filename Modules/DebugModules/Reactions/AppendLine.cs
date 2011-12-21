90452db7330502906cf56e9b95cdbbdcb37e722d
Mayhem <Mayhem@ASG-HPDev1.redmond.corp.microsoft.com>
2011-12-20 21:57:41 -0800

    Adding a reaction to append a line to the end of a file.

---
+++ Modules/DebugModules/Reactions/AppendLine.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+using MayhemCore;
+using System.Windows;
+using MayhemWpf.ModuleTypes;
+using System.IO;
+using DebugModules.Wpf;
+
+namespace DebugModules.Reactions
+{
+    [DataContract]
+    [MayhemModule("Append Line", "Appends a line to a file")]
+    public class AppendLine : ReactionBase, IWpfConfigurable
+    {
+        [DataMember]
+        private String LineToAdd;
+        [DataMember]
+        private String FilePath;
+
+        public override void Perform()
+        {
+
+            if (File.Exists(FilePath))
+            {
+                using (System.IO.StreamWriter file = new System.IO.StreamWriter(@FilePath, true))
+                {
+                    file.WriteLine(LineToAdd);
+                }
+            }
+            else
+            {
+                ErrorLog.AddError(ErrorType.Failure, "File does not exist");
+            }
+        }
+
+        public MayhemWpf.UserControls.WpfConfiguration ConfigurationControl
+        {
+            get { return new AppendLineConfig(LineToAdd, FilePath); }
+        }
+
+        public void OnSaved(MayhemWpf.UserControls.WpfConfiguration configurationControl)
+        {
+            LineToAdd = ((AppendLineConfig)configurationControl).Line;
+            FilePath = ((AppendLineConfig)configurationControl).File;
+        }
+
+        public string GetConfigString()
+        {
+            return "\"" + LineToAdd + "\" to File: " + Path.GetFileName(FilePath);
+        }
+    }
+}
---
+++ Modules/DebugModules/Wpf/AppendLineConfig.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MayhemWpf.UserControls;
+using Microsoft.Win32;
+
+namespace DebugModules.Wpf
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class AppendLineConfig : WpfConfiguration
+    {
+        private bool shouldCheckVisibility = false;
+        public string Line
+        {
+            get;
+            private set;
+        }
+
+        public string File
+        {
+            get;
+            private set;
+        }
+
+        public AppendLineConfig(string line, string file)
+        {
+            this.Line = line;
+            this.File = file;
+            InitializeComponent();
+        }
+
+        public override void OnLoad()
+        {
+            LineBox.Text = this.Line;
+            FileBox.Text = this.File;
+        }
+
+        public override void OnSave()
+        {
+            File = FileBox.Text.Trim();
+            Line = LineBox.Text.Trim();
+        }
+
+        private void checkFile(string File)
+        {
+            CanSave = File.Length > 0 && System.IO.File.Exists(FileBox.Text.Trim());
+        }
+
+        public override string Title
+        {
+            get
+            {
+                return "Add Line";
+            }
+        }
+
+        private void File_Changed(object sender, TextChangedEventArgs e)
+        {
+            checkFile(File);
+            fileInvalid.Visibility = CanSave ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        // Browse for file
+        private void Browse_Click(object sender, RoutedEventArgs e)
+        {
+            var dlg = new OpenFileDialog();
+
+            dlg.FileName = File;
+            dlg.DefaultExt = ".txt";
+
+            if (dlg.ShowDialog() == true)
+            {
+                File = dlg.FileName;
+                FileBox.Text = File;
+            }
+        }
+    }
+}
