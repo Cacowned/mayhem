0c0c74c25c653f4389d6a57c43686d7d6cf7da81
v-willbe <v-willbe@ASG-CHALMERS.redmond.corp.microsoft.com>
2012-01-26 16:16:44 -0800

    Checking in FeedAlert

---
+++ Modules/DefaultModules/Events/FeedAlert.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+using MayhemCore;
+using MayhemWpf.ModuleTypes;
+using System.Windows.Threading;
+using MayhemWpf.UserControls;
+using System.Runtime.InteropServices;
+using DefaultModules.Resources;
+using System.Xml;
+using DefaultModules.Wpf;
+
+namespace DefaultModules.Events
+{
+    [DataContract]
+    [MayhemModule("RSS Feed Alert", "Monitors changes in temperature")]
+
+    public class FeedAlert : EventBase, IWpfConfigurable
+    {
+        [DataMember]
+        private string zipCode;
+
+        [DataMember]
+        private int temperature;
+
+        [DataMember]
+        private bool checkBelow;
+
+        private bool hasPassed;
+        private DispatcherTimer timer;
+        private bool internetFlag;
+
+        public void OnSaved(WpfConfiguration configurationControl)
+        {
+            var config = (FeedAlertConfig)configurationControl;
+            /*
+            zipCode = config.ZipCodeProp;
+            temperature = config.TempProp;
+            checkBelow = config.CheckAbove;
+             * */
+        }
+
+        public WpfConfiguration ConfigurationControl
+        {
+            get { return new FeedAlertConfig(); }
+        }
+
+        public string GetConfigString()
+        {
+            return "configuring the feeds of this world... alert";
+        }
+
+        protected override void OnLoadDefaults()
+        {
+            zipCode = "98105";
+            temperature = 32;
+            checkBelow = true;
+        }
+
+        protected override void OnAfterLoad()
+        {
+            timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan(0, 1, 0);
+            timer.Tick += CheckFeed;
+            hasPassed = false;
+        }
+
+        #region Timer
+        protected override void OnEnabling(EnablingEventArgs e)
+        {
+            if (!ConnectedToInternet())
+            {
+                ErrorLog.AddError(ErrorType.Warning, Strings.Internet_NotConnected);
+            }
+            timer.Start();
+        }
+
+        protected override void OnDisabled(DisabledEventArgs e)
+        {
+            timer.Stop();
+            // when turned off then off again, will check for passing weather point
+            hasPassed = false;
+        }
+        #endregion
+
+        private void CheckFeed(object sender, EventArgs e)
+        {
+            // Test for internet connection
+            if (ConnectedToInternet())
+            {
+                internetFlag = true;
+                // Retrieve XML document  
+                XmlTextReader reader = new XmlTextReader("http://www.google.com/ig/api?weather=" + zipCode.Replace(" ", "%20"));
+                reader.WhitespaceHandling = WhitespaceHandling.Significant;
+
+                reader.ReadToFollowing("temp_f");
+                int temp = Convert.ToInt32(reader.GetAttribute("data"));
+
+                bool isBelowOrAbove = (checkBelow && temp >= temperature) || (!checkBelow && temp <= temperature);
+
+                // if below desired temperature and watching for below, trigger
+                // if above desired temperature and watching for abovem trigger
+                if (isBelowOrAbove)
+                {
+                    if (!hasPassed)
+                    {
+                        hasPassed = true;
+                        Trigger();
+                    }
+                    else if (temp == temperature)
+                    {
+                        hasPassed = false;
+                    }
+                }
+
+            }
+            else if (internetFlag)
+            {
+                internetFlag = false;
+                ErrorLog.AddError(ErrorType.Warning, Strings.Internet_NotConnected);
+            }
+        }
+
+        [DllImport("wininet.dll")]
+        private static extern bool InternetGetConnectedState(out int desciption, int reservedValue);
+
+        private static bool ConnectedToInternet()
+        {
+            int desc;
+            try
+            {
+                return InternetGetConnectedState(out desc, 0);
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Modules/DefaultModules/Wpf/FeedAlertConfig.xaml.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Threading;
+using System.Xml;
+using MayhemWpf.UserControls;
+using System.Runtime.InteropServices;
+
+namespace DefaultModules.Wpf
+{
+    public partial class FeedAlertConfig : WpfConfiguration
+    {
+
+        private DispatcherTimer timer;
+        private XmlTextReader stockData;
+
+        public override string Title
+        {
+            get
+            {
+                return "RSS Feed Alert";
+            }
+        }
+
+        public FeedAlertConfig()
+        {
+            timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan(0, 0, 2);
+            //timer.Tick += CheckInternet;
+
+            InitializeComponent();
+        }
+
+        public override void OnLoad()
+        {
+            //StockSymbol.Text = StockSymbolProp;
+        }
+
+        public override void OnSave()
+        {
+           // StockSymbolProp = StockSymbol.Text;
+        }
+
+        private void CheckInternet(object sender, EventArgs e)
+        {
+            // don't want to add overloads to ConnectedToInternet()
+            ConnectedToInternet();
+        }
+
+        private void VerifyFields()
+        {
+            string error = "Invalid";
+            IsValidStock();
+            CanSave = error.Equals("Invalid");
+            TextChanged(error);
+        }
+
+        private bool IsValidStock()
+        {
+            if (ConnectedToInternet())// && StockSymbol.Text.Length > 0)
+            {
+                stockData = new XmlTextReader("http://www.npr.org/rss/rss.php?id=1012");
+                XmlDocument rssDoc = new XmlDocument();
+                rssDoc.Load(stockData);
+
+                MessageBox.Show(rssDoc.ChildNodes.Count.ToString());
+
+                for (int i = 0; i < rssDoc.ChildNodes.Count; i++)
+                {
+                    if (rssDoc.ChildNodes[i].Name == "rss")
+                    {
+                        MessageBox.Show(rssDoc.ChildNodes[i].InnerText);
+                    }
+                    
+                }
+                /*
+                // check that there is xml data
+                stockData.ReadToFollowing("company");
+                if (stockData.GetAttribute("data").Equals(""))
+                {
+                    StockName.Text = "Stock";
+                    return false;
+                }
+                 */
+                return true;
+            }
+            return false;
+        }
+
+        private void TextChanged(string text)
+        {
+            textInvalid.Text = text;
+            textInvalid.Visibility = CanSave ? Visibility.Collapsed : Visibility.Visible;
+            if (textInvalid.Visibility == Visibility.Visible)
+            {
+                FeedName.Text = "Feed";
+            }
+            if (CanSave && !timer.IsEnabled)
+            {
+                //CheckStock();
+            }
+        }
+
+        private void CheckStock()
+        {
+            //stockData.ReadToFollowing("company");
+            string companyName = stockData.GetAttribute("data");
+
+            // get current stock price
+            stockData.ReadToFollowing("last");
+            string currentPrice = stockData.GetAttribute("data");
+
+            FeedName.Text = String.Format("{0} - ${1}", companyName, currentPrice);
+        }
+
+        [DllImport("wininet.dll")]
+        private static extern bool InternetGetConnectedState(out int desciption, int reservedValue);
+
+        private bool ConnectedToInternet()
+        {
+            int desc;
+            try
+            {
+                if (InternetGetConnectedState(out desc, 0))
+                {
+                    if (timer.IsEnabled)
+                    {
+                        timer.Stop();
+                        VerifyFields();
+                    }
+                    return true;
+                }
+            }
+            catch { }
+
+            if (!timer.IsEnabled)
+            {
+                timer.Start();
+            }
+            return false;
+        }
+
+        private void Stock_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            if (!timer.IsEnabled)
+            {
+                VerifyFields();
+            }
+            else
+            {
+                FeedName.Text = "Stock";
+                TextChanged("Cannot connect to the Internet");
+            }
+        }
+
+        private void URL_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
