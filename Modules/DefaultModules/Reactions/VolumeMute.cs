f270f9d6191ea9d7dece04d2f268a9133bc4edb9
Eli <mayhem@eli-white.com>
2012-01-31 15:20:26 -0800

    Added Volume Mute as a reaction to Default Modules

---
+++ Modules/DefaultModules/Reactions/VolumeMute.cs
@@ -0,0 +1,16 @@
+﻿using CoreAudioApi;
+using MayhemCore;
+
+namespace DefaultModules.Reactions
+{
+	[MayhemModule("Volume: Mute", "Mutes / unmutes the default audio device")]
+	public class VolumeMute : ReactionBase
+	{
+		public override void Perform()
+		{
+			MMDeviceEnumerator devEnum = new MMDeviceEnumerator();
+			MMDevice m_device = devEnum.GetDefaultAudioEndpoint(EDataFlow.eRender, ERole.eMultimedia);
+			m_device.AudioEndpointVolume.Mute = !m_device.AudioEndpointVolume.Mute;
+		}
+	}
+}
