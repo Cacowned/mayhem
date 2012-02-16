48ccc52c17a64eb5408c2aed61723f5233e97a75
v-willbe <v-willbe@ASG-CHALMERS.redmond.corp.microsoft.com>
2012-02-15 16:41:18 -0800

    Facebook WallPost reaction and UpdateStatus event are ready to be tested!

---
+++ Modules/FacebookModules/Wpf/StatusConfig.xaml.cs
@@ -0,0 +1,34 @@
+﻿using FacebookModules.Wpf.UserControls;
+
+namespace FacebookModules.Wpf
+{
+    /// <summary>
+    /// Interaction logic for StatusConfig.xaml
+    /// </summary>
+    public partial class StatusConfig : FacebookConfigControl
+    {
+        public string StatusProp
+        {
+            get;
+            private set;
+        }
+
+        public StatusConfig()
+        {
+            InitializeComponent();
+        }
+
+        public override string Title
+        {
+            get
+            {
+                return "Facebook - Status Update";
+            }
+        }
+
+        public override void OnSave()
+        {
+            StatusProp = statusText.Text;
+        }
+    }
+}
