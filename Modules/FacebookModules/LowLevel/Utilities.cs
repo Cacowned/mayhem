86884b42c94479b775e7a48abe1a06572a51df1b
v-willbe <v-willbe@ASG-CHALMERS.redmond.corp.microsoft.com>
2012-02-29 15:18:46 -0800

    Updated Facebook timeout and Internet Error

--- Modules/FacebookModules/LowLevel/Utilities.cs
+++ Modules/FacebookModules/LowLevel/Utilities.cs
@@ -1,7 +1,10 @@
-﻿namespace FacebookModules.LowLevel
+﻿using System;
+using System.Threading;
+namespace FacebookModules.LowLevel
 {
     public static class Utilities
     {
+        #region Check Internet Connection
         /// <summary>
         /// Checks if there is a current Internet connection
         /// </summary>
@@ -10,7 +13,8 @@
         {
             try
             {
-                System.Net.IPHostEntry obj = System.Net.Dns.GetHostEntry("www.google.com");
+                // Make a request with a 640ms timeout
+                CallWithTimeout(MakeRequest, 640);
                 return true;
             }
             catch
@@ -18,5 +22,42 @@
                 return false;
             }
         }
+
+        /// <summary>
+        /// Make a request to google.com
+        /// </summary>
+        private static void MakeRequest()
+        {
+            System.Net.IPHostEntry obj = System.Net.Dns.GetHostEntry("www.google.com");
+        }
+
+        /// <summary>
+        /// A method that will trigger a "timeout" error to be used when running a method with a 
+        /// specific timout trigger
+        /// </summary>
+        /// <param name="action">The method being run</param>
+        /// <param name="timeout">The timeout in milliseconds</param>
+        public static void CallWithTimeout(Action action, int timeout)
+        {
+            Thread threadToKill = null;
+            Action wrappedAction = () =>
+            {
+                threadToKill = Thread.CurrentThread;
+                action();
+            };
+
+            IAsyncResult result = wrappedAction.BeginInvoke(null, null);
+            if (result.AsyncWaitHandle.WaitOne(timeout))
+            {
+                wrappedAction.EndInvoke(result);
+                //throw new TimeoutException();
+            }
+            else
+            {
+                threadToKill.Abort();
+                throw new TimeoutException();
+            }
+        }
+        #endregion
     }
 }
