0435848092c2dfa70b59058353b8aa0465e850fe
Eli <mayhem@eli-white.com>
2012-03-12 15:44:57 -0700

    Working on the RFID event.

--- Modules/PhidgetModules/Events/Phidget1023RFID.cs
+++ Modules/PhidgetModules/Events/Phidget1023RFID.cs
@@ -9,69 +9,76 @@ using Phidgets.Events;
 
 namespace PhidgetModules.Events
 {
-    [DataContract]
-    [MayhemModule("Phidget: Rfid", "Triggers with a certain Rfid Tag")]
-    public class Phidget1023Rfid : EventBase, IWpfConfigurable
-    {
-        // This is the tag we are watching for
-        [DataMember]
-        private string tag;
+	[DataContract]
+	[MayhemModule("Phidget: Rfid", "Triggers with a certain Rfid Tag")]
+	public class Phidget1023Rfid : EventBase, IWpfConfigurable
+	{
+		// This is the tag we are watching for
+		[DataMember]
+		private string tag;
 
-        private RFID rfid;
+		private RFID rfid;
 
-        public WpfConfiguration ConfigurationControl
-        {
-            get { return new Phidget1023RFIDConfig(tag); }
-        }
+		public WpfConfiguration ConfigurationControl
+		{
+			get { return new Phidget1023RFIDConfig(tag); }
+		}
 
-        public void OnSaved(WpfConfiguration configurationControl)
-        {
-            tag = ((Phidget1023RFIDConfig)configurationControl).TagId;
-        }
+		public void OnSaved(WpfConfiguration configurationControl)
+		{
+			tag = ((Phidget1023RFIDConfig)configurationControl).TagId;
+		}
 
-        public string GetConfigString()
-        {
-            return string.Format("Rfid Tag ID {0}", tag);
-        }
+		public string GetConfigString()
+		{
+			return string.Format("Rfid Tag ID {0}", tag);
+		}
 
-        // Tag event handler...we'll display the tag code in the field on the GUI
-        private void RfidTag(object sender, TagEventArgs e)
-        {
-            if (e.Tag == tag)
-            {
-                Trigger();
-                rfid.LED = true;
-            }
-        }
-
-        // Tag event handler...we'll display the tag code in the field on the GUI
-        private void LostRfidTag(object sender, TagEventArgs e)
-        {
-            rfid.LED = false;
-        }
-
-        protected override void OnEnabling(EnablingEventArgs e)
-        {
-			try
+		// Tag event handler...we'll display the tag code in the field on the GUI
+		private void RfidTag(object sender, TagEventArgs e)
+		{
+			if (e.Tag == tag)
 			{
-				rfid = PhidgetManager.Get<RFID>();
+				Trigger();
+				rfid.LED = true;
 			}
-			catch (InvalidOperationException)
-			{
-				ErrorLog.AddError(ErrorType.Failure, "The RFID receiver is not attached");
-				e.Cancel = true;
-				return;
-			}
-			
-            rfid.Tag += RfidTag;
-            rfid.TagLost += LostRfidTag;
-        }
+		}
 
-        protected override void OnDisabled(DisabledEventArgs e)
-        {
+		// Tag event handler...we'll display the tag code in the field on the GUI
+		private void LostRfidTag(object sender, TagEventArgs e)
+		{
+			rfid.LED = false;
+		}
+
+		protected override void OnEnabling(EnablingEventArgs e)
+		{
+			if (!e.WasConfiguring)
+			{
+				try
+				{
+					rfid = PhidgetManager.Get<RFID>();
+				}
+				catch (InvalidOperationException)
+				{
+					ErrorLog.AddError(ErrorType.Failure, "The RFID receiver is not attached");
+					e.Cancel = true;
+					return;
+				}
+			}
+
+			rfid.Tag += RfidTag;
+			rfid.TagLost += LostRfidTag;
+		}
+
+		protected override void OnDisabled(DisabledEventArgs e)
+		{
 			rfid.Tag -= RfidTag;
 			rfid.TagLost -= LostRfidTag;
-			PhidgetManager.Release<RFID>(ref rfid);
-        }
-    }
+
+			if (!e.IsConfiguring)
+			{
+				PhidgetManager.Release<RFID>(ref rfid);
+			}
+		}
+	}
 }
