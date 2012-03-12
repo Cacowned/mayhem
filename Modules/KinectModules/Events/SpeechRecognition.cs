846b1e42b7ae67334ce3ade9e8678e0fea7f6601
Eli <Eli@Eli-Desktop.Home>
2012-03-11 23:51:34 -0700

    Adding Kinect managers in. Modifying the Kinect speech recog to try and support having multiple connections with it running at the same time.

--- Modules/KinectModules/Events/SpeechRecognition.cs
+++ Modules/KinectModules/Events/SpeechRecognition.cs
@@ -19,74 +19,23 @@ namespace KinectModules.Events
 		[DataMember]
 		private string phrase;
 
-		private KinectSensor sensor;
-
-		private SpeechRecognitionEngine engine;
-
-		private Stream stream;
-
 		protected override void OnEnabling(EnablingEventArgs e)
 		{
-			sensor = (from sensorToCheck in KinectSensor.KinectSensors where sensorToCheck.Status == KinectStatus.Connected select sensorToCheck).FirstOrDefault();
-			if (sensor == null)
-			{
-				ErrorLog.AddError(ErrorType.Failure, "Kinect not pluged in");
-				e.Cancel = true;
-				return;
-			}
-
-			sensor.Start();
-
-			KinectAudioSource source = sensor.AudioSource;
-			source.EchoCancellationMode = EchoCancellationMode.None;
-			source.AutomaticGainControlEnabled = false;
-
-			RecognizerInfo ri = GetKinectRecognizer();
-
-			if (ri == null)
-			{
-				ErrorLog.AddError(ErrorType.Failure, "Could not find Kinect speech recognizer");
-				e.Cancel = true;
-				return;
-			}
-
 			try
 			{
-				engine = new SpeechRecognitionEngine(ri.Id);
+				SpeechRecognitionManager.Recognize(phrase, Recognized);
 			}
-			catch
+			catch (InvalidOperationException ex)
 			{
-				ErrorLog.AddError(ErrorType.Failure, "No Kinect found");
+				ErrorLog.AddError(ErrorType.Failure, ex.Message);
 				e.Cancel = true;
 				return;
 			}
-
-			var choices = new Choices();
-			choices.Add(phrase);
-
-			var gb = new GrammarBuilder { Culture = ri.Culture };
-			gb.Append(choices);
-
-
-			Grammar grammar = new Grammar(gb);
-			
-			engine.LoadGrammar(grammar);
-			engine.SpeechRecognized += RecognitionEngine_SpeechRecognized;
-
-
-			stream = source.Start();
-			engine.SetInputToAudioStream(stream,
-					new SpeechAudioFormatInfo(
-						EncodingFormat.Pcm, 16000, 16, 1,
-						32000, 2, null));
-
-			engine.RecognizeAsync(RecognizeMode.Multiple);
 		}
 
 		protected override void OnDisabled(DisabledEventArgs e)
 		{
-			engine.RecognizeAsyncCancel();
-			sensor.Stop();
+			SpeechRecognitionManager.CancelRecognize(phrase, Recognized);
 		}
 
 		public WpfConfiguration ConfigurationControl
@@ -105,24 +54,13 @@ namespace KinectModules.Events
 			return phrase;
 		}
 
-		private void RecognitionEngine_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+		private void Recognized(SpeechRecognizedEventArgs e)
 		{
 			Logger.WriteLine("Said: " + e.Result.Text + ", with " + e.Result.Confidence + " confidence");
-			if (e.Result.Confidence > .85)
+			if (e.Result.Confidence > .5)
 			{
 				Trigger();
 			}
-		}
-
-		private static RecognizerInfo GetKinectRecognizer()
-		{
-			Func<RecognizerInfo, bool> matchingFunc = r =>
-			{
-				string value;
-				r.AdditionalInfo.TryGetValue("Kinect", out value);
-				return "True".Equals(value, StringComparison.InvariantCultureIgnoreCase) && "en-US".Equals(r.Culture.Name, StringComparison.InvariantCultureIgnoreCase);
-			};
-			return SpeechRecognitionEngine.InstalledRecognizers().Where(matchingFunc).FirstOrDefault();
 		}
 	}
 }
