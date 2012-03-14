ba19845f1aa662cf3a90faa27ed52aff253f34e2
Eli <Eli@Eli-Desktop.Home>
2012-03-13 20:06:58 -0700

    Rewriting the MayhemSerial add-on to be more C# consistent and manage multiple different events trying to talk on the same port (or listen).
    Adding a SerialModules add-on that can be used to trigger when a string is sent over serial, or to send a string over serial.

---
+++ Modules/MayhemSerial/SerialSettings.cs
@@ -0,0 +1,96 @@
+﻿using System.IO.Ports;
+using System;
+
+namespace MayhemSerial
+{
+	public class SerialSettings
+	{
+		public int BaudRate
+		{
+			get;
+			private set;
+		}
+
+		public Parity Parity
+		{
+			get;
+			private set;
+		}
+
+		public StopBits StopBits
+		{
+			get;
+			private set;
+		}
+
+		public int DataBits
+		{
+			get;
+			private set;
+		}
+
+		public SerialSettings(int baudRate, Parity parity, StopBits stopBits, int dataBits)
+		{
+			BaudRate = baudRate;
+			Parity = parity;
+			StopBits = stopBits;
+			DataBits = dataBits;
+		}
+
+		#region Overriding Equals and related items
+		public override bool Equals(object obj)
+		{
+			if (obj == null)
+				return false;
+
+			SerialSettings other = obj as SerialSettings;
+			if (other == null)
+				return false;
+
+			return Equals(other);
+		}
+
+		public bool Equals(SerialSettings obj)
+		{
+			if (obj == null)
+				return false;
+
+			return BaudRate == obj.BaudRate &&
+					Parity == obj.Parity &&
+					StopBits == obj.StopBits &&
+					DataBits == obj.DataBits;
+		}
+
+		public override int GetHashCode()
+		{
+			return BaudRate ^ Parity.GetHashCode() ^ StopBits.GetHashCode() ^ DataBits;
+		}
+
+		public static bool operator ==(SerialSettings a, SerialSettings b)
+		{
+			// If both are null, or both are same instance, return true.
+			if (Object.ReferenceEquals(a, b))
+			{
+				return true;
+			}
+
+			// If one is null, but not both, return false.
+			if (((object)a == null) || ((object)b == null))
+			{
+				return false;
+			}
+
+			// Return true if the fields match:
+			return a.BaudRate == b.BaudRate &&
+					a.Parity == b.Parity &&
+					a.StopBits == b.StopBits &&
+					a.DataBits == b.DataBits;
+		}
+
+		public static bool operator !=(SerialSettings a, SerialSettings b)
+		{
+			return !(a == b);
+		}
+		#endregion
+	}
+}
---
+++ Modules/SerialModules/Events/SerialListen.cs
@@ -0,0 +1,39 @@
+﻿using System.IO.Ports;
+using System.Runtime.Serialization;
+using System.Text;
+using MayhemCore;
+using MayhemSerial;
+
+namespace SerialModules.Events
+{
+	[DataContract]
+	[MayhemModule("Serial Listen", "Listens Serially")]
+	public class SerialListen : EventBase
+	{
+		SerialPortManager manager;
+		SerialSettings settings;
+
+		protected override void OnAfterLoad()
+		{
+			manager = SerialPortManager.Instance;
+			settings = new SerialSettings(9600, Parity.Even, StopBits.One, 8);
+		}
+
+		protected override void OnEnabling(EnablingEventArgs e)
+		{
+			manager.ConnectPort("COM7", settings, RecievedData);
+		}
+
+		protected override void OnDisabled(DisabledEventArgs e)
+		{
+			manager.ReleasePort("COM7", RecievedData);
+		}
+
+		private void RecievedData(byte[] bytes, int numBytes) 
+		{
+			string str = Encoding.UTF8.GetString(bytes);
+
+			Logger.WriteLine(str);
+		}
+	}
+}
