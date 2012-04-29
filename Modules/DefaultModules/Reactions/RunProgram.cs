6be0827d6f6fd3bd1f56700c95bc2bb1174edf93
Eli <Eli@Eli-Desktop.Home>
2012-04-29 00:59:15 -0700

    RunProgram now launches the application from it's working directory instead of Mayhem's working directory.

--- Modules/DefaultModules/Reactions/RunProgram.cs
+++ Modules/DefaultModules/Reactions/RunProgram.cs
@@ -6,70 +6,76 @@ using DefaultModules.Wpf;
 using MayhemCore;
 using MayhemWpf.ModuleTypes;
 using MayhemWpf.UserControls;
+using System.Diagnostics;
 
 namespace DefaultModules.Reactions
 {
-    [DataContract]
-    [MayhemModule("Run Program", "Runs a given program")]
-    public class RunProgram : ReactionBase, IWpfConfigurable
-    {
-        [DataMember]
-        private string FileName
-        {
-            get;
-            set;
-        }
+	[DataContract]
+	[MayhemModule("Run Program", "Runs a given program")]
+	public class RunProgram : ReactionBase, IWpfConfigurable
+	{
+		[DataMember]
+		private string FileName
+		{
+			get;
+			set;
+		}
 
-        [DataMember]
-        private string Arguments
-        {
-            get;
-            set;
-        }
+		[DataMember]
+		private string Arguments
+		{
+			get;
+			set;
+		}
 
-        public override void Perform()
-        {
-            if (File.Exists(FileName))
-            {
-                try
-                {
-                    System.Diagnostics.Process.Start(FileName, Arguments);
-                }
-                catch
-                {
-                    ErrorLog.AddError(ErrorType.Failure, Strings.RunProgram_CantStartProgram);
-                }
-            }
-            else
-            {
-                ErrorLog.AddError(ErrorType.Failure, Strings.RunProgram_FileNotFound);
-            }
-        }
+		public override void Perform()
+		{
+			if (File.Exists(FileName))
+			{
+				try
+				{
+					ProcessStartInfo startInfo = new ProcessStartInfo();
+					startInfo.WorkingDirectory = new DirectoryInfo(FileName).Parent.FullName;
+					startInfo.FileName = FileName;
+					startInfo.Arguments = Arguments;
+					
+					Process.Start(startInfo);
+				}
+				catch
+				{
+					ErrorLog.AddError(ErrorType.Failure, Strings.RunProgram_CantStartProgram);
+				}
+			}
+			else
+			{
+				ErrorLog.AddError(ErrorType.Failure, Strings.RunProgram_FileNotFound);
+			}
+		}
 
-        public WpfConfiguration ConfigurationControl
-        {
-            get { return new RunProgramConfig(FileName, Arguments); }
-        }
+		public WpfConfiguration ConfigurationControl
+		{
+			get { return new RunProgramConfig(FileName, Arguments); }
+		}
 
-        public void OnSaved(WpfConfiguration configurationControl)
-        {
-            var rpc = configurationControl as RunProgramConfig;
-            FileName = rpc.Filename;
-            Arguments = rpc.Arguments;
-        }
+		public void OnSaved(WpfConfiguration configurationControl)
+		{
+			var rpc = configurationControl as RunProgramConfig;
+			FileName = rpc.Filename;
+			Arguments = rpc.Arguments;
+		}
 
-        protected override void OnEnabling(EnablingEventArgs e)
-        {
-            if (!File.Exists(FileName))
-            {
-                ErrorLog.AddError(ErrorType.Failure, Strings.RunProgram_FileNotFound);
-                e.Cancel = true;
-            }
-        }
+		protected override void OnEnabling(EnablingEventArgs e)
+		{
+			if (!File.Exists(FileName))
+			{
+				ErrorLog.AddError(ErrorType.Failure, Strings.RunProgram_FileNotFound);
+				e.Cancel = true;
+			}
+		}
 
-        public string GetConfigString()
-        {
-            return string.Format(CultureInfo.CurrentCulture, Strings.RunProgram_ConfigString, Path.GetFileName(FileName));
-        }
-    }
+		public string GetConfigString()
+		{
+			return string.Format(CultureInfo.CurrentCulture, Strings.RunProgram_ConfigString, Path.GetFileName(FileName));
+		}
+	}
 }
