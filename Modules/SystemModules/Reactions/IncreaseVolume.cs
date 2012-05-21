c71e84f6a47d7b51ef8a07e63ffcea170499ad5d
Anirudh Ranganath <Anirudh Ranganath@localhost>
2012-05-21 23:21:30 +0530

    Added Suspend and Hibernate Reactions. Created folder hierarchy and renamed namespaces accordingly.

---
+++ Modules/SystemModules/Reactions/IncreaseVolume.cs
@@ -0,0 +1,30 @@
+﻿using System.Runtime.InteropServices;
+using System.Runtime.Serialization;
+using CoreAudioApi;
+using MayhemCore;
+
+namespace SystemModules.Reactions
+{
+	[DataContract]
+	[MayhemModule("Increase Volume", "Increases the master volume level")]
+	public class IncreaseVolume : ReactionBase
+	{
+		public override void Perform()
+		{
+			MMDeviceEnumerator devEnum = new MMDeviceEnumerator();
+			MMDevice device;
+
+			try
+			{
+				device = devEnum.GetDefaultAudioEndpoint(EDataFlow.eRender, ERole.eMultimedia);
+			}
+			catch (COMException)
+			{
+				ErrorLog.AddError(ErrorType.Failure, "No audio output device available.");
+				return;
+			}
+
+			device.AudioEndpointVolume.VolumeStepUp();
+		}
+	}
+}
---
+++ Modules/SystemModules/Reactions/MuteVolume.cs
@@ -0,0 +1,29 @@
+﻿using System.Runtime.InteropServices;
+using System.Runtime.Serialization;
+using CoreAudioApi;
+using MayhemCore;
+
+namespace SystemModules.Reactions
+{
+	[MayhemModule("Mute Volume", "Mutes / unmutes the default audio device")]
+	public class MuteVolume : ReactionBase
+	{
+		public override void Perform()
+		{
+			MMDeviceEnumerator devEnum = new MMDeviceEnumerator();
+			MMDevice device;
+
+			try
+			{
+				device = devEnum.GetDefaultAudioEndpoint(EDataFlow.eRender, ERole.eMultimedia);
+			}
+			catch (COMException)
+			{
+				ErrorLog.AddError(ErrorType.Failure, "No audio output device available.");
+				return;
+			}
+
+			device.AudioEndpointVolume.Mute = !device.AudioEndpointVolume.Mute;
+		}
+	}
+}
