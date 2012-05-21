ba08863c3571fb75ce21078575101b0c75dba4b6
Anirudh Ranganath <Anirudh Ranganath@localhost>
2012-05-22 03:50:15 +0530

    Added shutdown Reaction to SystemModules. Cleaned SetUpVolumeConfig's usings.

---
+++ Modules/SystemModules/Wpf/ShutDownWarning.xaml.cs
@@ -0,0 +1,35 @@
+﻿using MayhemWpf.UserControls;
+
+namespace SystemModules.Wpf
+{
+    /// <summary>
+    /// Interaction logic for ShutDownWarning.xaml
+    /// </summary>
+    public partial class ShutDownWarning : WpfConfiguration
+    {
+        public bool _forceShutDown 
+        { 
+            get;
+            private set;
+        }
+        public ShutDownWarning(bool forceShutDown = false)
+        {
+            InitializeComponent();
+            CanSave = true;
+            this._forceShutDown = forceShutDown;
+        }
+        public override string Title
+        {
+            get{return ("Warning");}
+        }
+        public override void OnLoad()
+        {
+            WarningBox.Text = SystemModules.Resources.Strings.Shutdown_ForceWarning;
+            EnableForceCheckBox.IsChecked = _forceShutDown;
+        }
+        public override void OnSave()
+        {
+            this._forceShutDown = (bool)EnableForceCheckBox.IsChecked;
+        }
+    }
+}
