f532dba4d04976b2a5cc49446d057697e2b335c9
thesavior <thesavior@localhost>
2012-05-28 23:59:22 -0700

    Fixing some references in the Serial related projects. Updating the serial nuspecs.

--- Modules/SerialManager/SerialPortManager.cs
+++ Modules/SerialManager/SerialPortManager.cs
@@ -1,11 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO.Ports;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Tasks;
-using System.Diagnostics;
 
 namespace SerialManager
 {
