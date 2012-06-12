3197d92377f14823c027d0bd7b15390955dae81a
t-greyes <t-greyes@MSFT-152281.redmond.corp.microsoft.com>
2012-06-12 16:57:01 -0700

    Adding new version of ArduinoModules (NOT WORKING)

--- Modules/ArduinoModules/Properties/AssemblyInfo.cs
+++ Modules/ArduinoModules/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ArduinoModules")]
+[assembly: AssemblyTitle("TemplateModules")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Outercurve")]
-[assembly: AssemblyProduct("ArduinoModules")]
+[assembly: AssemblyProduct("TemplateModules")]
 [assembly: AssemblyCopyright("Copyright © Outercurve 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -19,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("9bd2f4a7-e342-4b4b-a2b5-1d4d2875c331")]
+[assembly: Guid("fd8c521a-2262-44ba-a175-69c0cf4999ea")]
 
 // Version information for an assembly consists of the following four values:
 //
