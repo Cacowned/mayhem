e7b0299a35ee2eeba1622d2303cf9aedc0858789
t-greyes <t-greyes@MSFT-152281.redmond.corp.microsoft.com>
2012-06-12 16:57:01 -0700

    Adding new version of ArduinoModules (NOT WORKING)

--- Modules/ArduinoModules/Reactions/ArduinoAnalogWrite.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ArduinoModules.Reactions
-{
-    /// <summary>
-    /// Writes PWM to analog-out pins of the Arduino
-    /// </summary>
-    internal class ArduinoAnalogWrite
-    {
-    }
-}
