94a399d908548ea0cf2bcdacfd8edf44c65d26d5
t-greyes <t-greyes@MSFT-152281.redmond.corp.microsoft.com>
2012-06-19 23:32:16 -0700

    Added flash firmware support and made updates to MSP430Modules

--- Modules/MSP430Modules/Wpf/MSP430DigitalOutputConfig.xaml.cs
+++ Modules/MSP430Modules/Wpf/MSP430DigitalOutputConfig.xaml.cs
@@ -1,22 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO.Ports;
-using System.Linq;
-using System.Text;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Windows.Threading;
 using MayhemWpf.UserControls;
-using MSP430Modules;
-using MSP430Modules.Events;
 using MSP430Modules.Reactions;
+using System.Diagnostics;
+using System.Reflection;
+using System.Management;
 
 namespace MSP430Modules.Wpf
 {
@@ -29,105 +17,132 @@ namespace MSP430Modules.Wpf
         public int Index { get; set; }
         public string Port { get; set; }
         public string[] Ports { get; set; }
-        
-        public MSP430DigitalOutputConfig (int index, DigitalOutputType outputType, string port)
+
+        public MSP430DigitalOutputConfig(int index, DigitalOutputType outputType, string port)
         {
             OutputType = outputType;
             Index = index;
             Port = port;
-            
-            CanSave = true;            
+
+            CanSave = true;
             InitializeComponent();
         }
 
         public override void OnLoad()
         {
             PopulateOutputs();
-            //CheckCanSave();
 
+            // Output:
+            // Load the default output pin
             OutputBox.SelectedIndex = Index;
-            
+
+            // Trigger Type:
+            // Load the default action
             switch (OutputType)
             {
                 case DigitalOutputType.Toggle: ControlBox.SelectedIndex = 0;
                     break;
-                case DigitalOutputType.On: ControlBox.SelectedIndex = 1;
+                case DigitalOutputType.High: ControlBox.SelectedIndex = 1;
                     break;
-                case DigitalOutputType.Off: ControlBox.SelectedIndex = 2;
+                case DigitalOutputType.Low: ControlBox.SelectedIndex = 2;
                     break;
             }
-            
-            PortBox.SelectedItem = Port;
+
+            // COM Port:
+            // Load the COM port that contains the term "MSP430"            
+            foreach (object port in PortBox.Items)
+            {
+                if (port.ToString().Contains("MSP430"))
+                {
+                    PortBox.SelectedItem = port;
+                    string[] words = port.ToString().Split('-');
+                    Port = words[0].Trim();
+                }
+            }
         }
 
         private void PopulateOutputs()
         {
-            //if (ifKit.Attached)
-            //{
-            //    Dispatcher.Invoke(DispatcherPriority.Normal, (System.Action)(() =>
-            //    {
-            //        for (int i = 0; i < ifKit.outputs.Count; i++)
-            //        {
-                           
-            //        }
-            //    }));
-            //}
-            
+            // Input:
             // Add digital output pins as specified in MSP430 firmware
             OutputBox.Items.Add("P1.0");        // LED1
-            OutputBox.Items.Add("P1.4");        // GPIO pin
-            OutputBox.Items.Add("P1.5");        // GPIO pin
-            OutputBox.Items.Add("P1.6");        // GPIO pin
-            OutputBox.Items.Add("P1.7");        // GPIO pin
-            OutputBox.Items.Add("P2.0");        // GPIO pin
-            
-            // Get serial ports available for selection
-            string[] ports = SerialManager.SerialPortManager.AllPorts;
+            OutputBox.Items.Add("P1.4");
+            OutputBox.Items.Add("P1.5");
+            OutputBox.Items.Add("P1.6");        // LED2
+            OutputBox.Items.Add("P1.7");
+            OutputBox.Items.Add("P2.0");
 
-            if (ports.Length.Equals(0))
+            // Trigger Type:
+            // The available actions are added in the .xaml file
+
+            // COM Port:
+            // Add COM port to the configuration                                   
+            try
             {
-                PortBox.Items.Add("There are no available COM ports");
+                ObjectQuery query = new ObjectQuery("Select * from Win32_SerialPort");
+                ManagementObjectSearcher searcher = new ManagementObjectSearcher(query);
+                var result = searcher.Get();
+
+                if (result != null)
+                {
+                    foreach (ManagementObject obj in searcher.Get())
+                    {
+                        PortBox.Items.Add(obj["DeviceID"] + " - " + obj["Description"]);
+                    }
+                }
+
+                else
+                {
+                    PortBox.Items.Add("There are no available COM ports");
+                }
             }
 
-            else
+            catch
             {
-                foreach (string port in ports)
-                {
-                    PortBox.Items.Add(port);
-                }
+
             }
         }
 
         private void OutputBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            // Select the index of the output pin selected
             ComboBox box = sender as ComboBox;
             Index = box.SelectedIndex;
         }
 
         private void PortBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            // Select the item for the COM Port selected
             ComboBox box = sender as ComboBox;
-            Port = box.SelectedItem.ToString();
-            Console.Write(Port);
+
+            // Split the COM Port description and extract the "COM#" 
+            string[] words = box.SelectedItem.ToString().Split('-');
+            Port = words[0].Trim();
         }
 
         public override void OnSave()
         {
+            // Output:
+            // Save the selected output pin
             Index = OutputBox.SelectedIndex;
 
+            // Trigger Type:
+            // Save the selected action
             ComboBoxItem outputBox = ControlBox.SelectedItem as ComboBoxItem;
             switch (outputBox.Content.ToString())
             {
                 case "Toggle": OutputType = DigitalOutputType.Toggle;
                     break;
-                case "Turn On": OutputType = DigitalOutputType.On;
+                case "Turn High": OutputType = DigitalOutputType.High;
                     break;
-                case "Turn Off": OutputType = DigitalOutputType.Off;
+                case "Turn Low": OutputType = DigitalOutputType.Low;
                     break;
             }
-            
-            int selectedPort = PortBox.SelectedIndex;
-            Port = SerialManager.SerialPortManager.AllPorts[selectedPort];            
+
+            // COM Port:
+            // Split the COM Port description and save the "COM#" 
+            string[] words = PortBox.SelectedItem.ToString().Split('-');
+            Port = words[0].Trim();
         }
 
         public override string Title
@@ -140,7 +155,10 @@ namespace MSP430Modules.Wpf
 
         private void FlashButton_Click(object sender, RoutedEventArgs e)
         {
-            System.Diagnostics.Process.Start(@"C:\Users\t-greyes\Desktop\Mayhem\Modules\MSP430Modulesv2\Flasher\FlashMSP430.bat");
+            Assembly execAssembly = Assembly.GetExecutingAssembly();
+            string loc = execAssembly.Location;
+            string directory = System.IO.Path.GetDirectoryName(loc);
+            Process.Start(@directory + "/Flasher/FlashMSP430.bat");
         }
     }
 }
