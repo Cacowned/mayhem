d59030d976cae4421971d162b5bd0d0fd64aa05e
garr1985 <garr1985@localhost>
2013-02-15 09:58:45 -0500

    Added HttpGet Request reaction to DefaultModules

---
+++ Modules/DefaultModules/Wpf/HttpGetConfig.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using MayhemWpf.UserControls;
+
+namespace DefaultModules.Wpf
+{
+    public partial class HttpGetConfig : WpfConfiguration
+    {
+        public string Url
+        {
+            get;
+            private set;
+        }
+
+        public HttpGetConfig(string url)
+        {
+            Url = url;
+
+            InitializeComponent();
+        }
+
+        public override string Title
+        {
+            get { return "URL address"; }
+        }
+
+        public override void OnLoad()
+        {
+            UrlBox.Text = Url;
+        }
+
+        public override void OnSave()
+        {
+            Url = UrlBox.Text;
+        }
+
+        private void CheckValidity()
+        {
+            CanSave = UrlBox.Text.Length > 0;
+        }
+        
+        private void UrlBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            CheckValidity();
+
+            textInvalid.Visibility = CanSave ? Visibility.Collapsed : Visibility.Visible;
+        }
+    }
+}
